711ab9dcec87970652fa77a6f70b4f6dca7a4cd1
jingbin <770413277@qq.com>
2019-08-20 21:42:51 +0800

    init project

---
+++ app/src/main/java/me/jingbin/jrecyclerview/MainActivity.java
@@ -0,0 +1,13 @@
+package me.jingbin.jrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
